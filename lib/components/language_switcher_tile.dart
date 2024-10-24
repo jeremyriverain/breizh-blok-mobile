abca92e36e577fe12adb501aacb98842a21ede26
Jérémy Riverain <riverainjeremy@gmail.com>
2024-10-24 22:30:03 +0200

    feat: :lipstick: update language switcher tile

--- lib/components/language_switcher_tile.dart
+++ lib/components/language_switcher_tile.dart
@@ -23,31 +23,42 @@ class LanguageSwitcherTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: const Icon(Icons.language),
-      title: const Text('Langage'),
-      trailing: DropdownButtonHideUnderline(
-        child: DropdownButton(
-          value: currentLocale,
-          icon: const Icon(Icons.arrow_drop_down),
-          onChanged: (String? newValue) {},
-          items: languages.entries.map((entry) {
-            return DropdownMenuItem(
-              value: entry.key,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SvgPicture.asset(
-                    entry.value['flag']!,
-                    width: 24,
-                    height: 16,
-                  ),
-                  const SizedBox(width: 8),
-                  Text(entry.value['name']!),
-                ],
-              ),
-            );
-          }).toList(),
+      leading: DropdownMenu(
+        width: 180,
+        menuHeight: 120,
+        inputDecorationTheme: const InputDecorationTheme(
+          isDense: true,
+          border: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          contentPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
         ),
+        label: const Text('Langage'),
+        initialSelection: currentLocale,
+        onSelected: (String? newValue) {},
+        leadingIcon: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(width: 8),
+            SvgPicture.asset(
+              languages[currentLocale]!['flag']!,
+              width: 27,
+              height: 18,
+            ),
+          ],
+        ),
+        dropdownMenuEntries:
+            languages.entries.map<DropdownMenuEntry<String>>((entry) {
+          return DropdownMenuEntry<String>(
+            value: entry.key,
+            label: entry.value['name']!,
+            leadingIcon: SvgPicture.asset(
+              entry.value['flag']!,
+              width: 24,
+              height: 16,
+            ),
+          );
+        }).toList(),
       ),
     );
   }
