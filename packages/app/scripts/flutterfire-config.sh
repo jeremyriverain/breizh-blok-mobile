c121b6cf0295f458f821e87fbf6e30d837a043bf
Jérémy Riverain <riverainjeremy@gmail.com>
2025-11-25 07:08:53 +0100

    setup firebase - BB-125

--- packages/app/scripts/flutterfire-config.sh
+++ packages/app/scripts/flutterfire-config.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 # Script to generate Firebase configuration files for different environments/flavors
 
+set -e
+
+# Parse arguments
+for i in "$@"
+do
+case $i in
+    --staging-project=*)
+    STAGING_PROJECT="${i#*=}"
+    shift # past argument=value
+    ;;
+    --prod-project=*)
+    PROD_PROJECT="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+if [ -z "$STAGING_PROJECT" ] || [ -z "$PROD_PROJECT" ]
+then
+      echo "Usage: ./scripts/flutterfire-config.sh --staging-project=<staging_project_id> --prod-project=<prod_project_id>"
+      exit 1
+fi
+
 fvm dart pub global activate flutterfire_cli
 
 fvm dart pub global run flutterfire_cli:flutterfire configure \
       --yes \
       --platforms=android,ios \
-      --project={todo: replace with --staging-project arg} \
+      --project=$STAGING_PROJECT \
       --out=lib/firebase_options_staging.dart \
       --ios-bundle-id=fr.geekco.boulders.staging \
       --ios-out=ios/flavors/staging/GoogleService-Info.plist \
@@ -18,7 +44,7 @@ fvm dart pub global run flutterfire_cli:flutterfire configure \
 fvm dart pub global run flutterfire_cli:flutterfire configure \
       --yes \
       --platforms=android,ios \
-      --project={todo: replace with --prod-project arg} \
+      --project=$PROD_PROJECT \
       --out=lib/firebase_options_prod.dart \
       --ios-bundle-id=fr.geekco.boulders \
       --ios-out=ios/flavors/prod/GoogleService-Info.plist \
