536079d53133066ed31074a7eea853f8482b1f28
Jérémy Riverain <riverainjeremy@gmail.com>
2026-01-30 21:27:26 +0100

    :children_crossing: show to the user the error coming from the API - BB-183

--- packages/app/lib/ui/boulder/view_models/boulder_video_link_feedback_view_model.dart
+++ packages/app/lib/ui/boulder/view_models/boulder_video_link_feedback_view_model.dart
@@ -52,8 +52,6 @@ class BoulderVideoLinkFeedbackViewModel
               propertyPath: 'videoLink',
             );
 
-            print(videoLinkViolation);
-
             if (videoLinkViolation != null) {
               state.form
                   .control(
