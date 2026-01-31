d2a2036c5e9e91b11514aa5ddef625acc0e0612e
Jérémy Riverain <riverainjeremy@gmail.com>
2026-01-31 09:55:16 +0100

    :globe_with_meridians: retrieve translated content from API by passing Accept-language header - BB-188

--- packages/app/lib/data/data_sources/api/dio/create_dio.dart
+++ packages/app/lib/data/data_sources/api/dio/create_dio.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:breizh_blok_auth/breizh_blok_auth.dart';
 import 'package:breizh_blok_mobile/config/env.dart';
 import 'package:breizh_blok_mobile/data/data_sources/api/dio/bearer_interceptor.dart';
@@ -5,7 +7,7 @@ import 'package:dio/dio.dart';
 import 'package:dio_http2_adapter/dio_http2_adapter.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
-Dio _createBasicDio() {
+Dio _createBasicDio({required Locale locale}) {
   return Dio()
     ..httpClientAdapter = Http2Adapter(
       ConnectionManager(idleTimeout: const Duration(seconds: 10)),
@@ -15,14 +17,15 @@ Dio _createBasicDio() {
       headers: {
         'Accept': 'application/ld+json',
         'Content-type': 'application/ld+json',
+        'Accept-language': locale.languageCode.toLowerCase(),
       },
     )
     ..interceptors.add(PrettyDioLogger(requestHeader: true, requestBody: true));
 }
 
-Dio createDio({required Auth auth}) {
-  final dio = _createBasicDio();
-  final dio2 = _createBasicDio();
+Dio createDio({required Auth auth, required Locale locale}) {
+  final dio = _createBasicDio(locale: locale);
+  final dio2 = _createBasicDio(locale: locale);
 
   return dio..interceptors.add(BearerInterceptor(auth: auth, dio: dio2));
 }
