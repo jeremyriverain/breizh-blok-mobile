77d3aa3b4adb1dc07104a56954330ea30d060795
Jérémy Riverain <riverainjeremy@gmail.com>
2026-05-11 21:43:58 +0200

    w

--- packages/app/lib/ui/core/widgets/boulder_list_builder_filter_grade.dart
+++ packages/app/lib/ui/core/widgets/boulder_list_builder_filter_grade.dart
@@ -4,142 +4,151 @@ import 'package:breizh_blok_mobile/i18n/app_localizations.dart';
 import 'package:breizh_blok_mobile/ui/boulder/view_models/boulder_filter_grade_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class BoulderListBuilderFilterGrade extends StatefulWidget {
+part 'boulder_list_builder_filter_grade.g.dart';
+
+class BoulderListBuilderFilterGrade extends StatelessWidget {
   const BoulderListBuilderFilterGrade({required this.allGrades, super.key});
 
   final PaginatedCollection<Grade> allGrades;
 
   @override
-  State<BoulderListBuilderFilterGrade> createState() =>
-      _BoulderListBuilderFilterGradeState();
-}
-
-class _BoulderListBuilderFilterGradeState
-    extends State<BoulderListBuilderFilterGrade> {
-  late RangeValues _currentRangeValues;
-
-  @override
-  void initState() {
-    super.initState();
-    final selectedGrades =
-        context.read<BoulderFilterGradeBloc>().state.grades.toList()
-          ..sort((a, b) => a.name.compareTo(b.name));
-
-    if (selectedGrades.isEmpty) {
-      _currentRangeValues = RangeValues(
-        0,
-        (widget.allGrades.items.length - 1).toDouble(),
-      );
-    } else {
-      final startIndex = widget.allGrades.items.indexWhere(
-        (element) => element.iri == selectedGrades.first.iri,
-      );
-      final endIndex = widget.allGrades.items.indexWhere(
-        (element) => element.iri == selectedGrades.last.iri,
-      );
-      if (startIndex != -1 && endIndex != -1) {
-        _currentRangeValues = RangeValues(
-          startIndex.toDouble(),
-          endIndex.toDouble(),
-        );
-      } else {
-        _currentRangeValues = RangeValues(
-          0,
-          (widget.allGrades.items.length - 1).toDouble(),
-        );
-      }
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (widget.allGrades.totalItems < 2) {
+    if (allGrades.totalItems < 2) {
       // defensive condition, if the API returns less than 2 grades,
       // then RangeSlider widget won't work
       return Container();
     }
 
-    return Column(
-      children: [
-        ListTile(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text.rich(
-                TextSpan(
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: '${AppLocalizations.of(context).minGrade}: ',
-                    ),
-                    TextSpan(
-                      text: widget
-                          .allGrades
-                          .items[(_currentRangeValues.start as num).toInt()]
-                          .name,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                key: const Key('boulder-list-grade-min'),
-              ),
-              Text.rich(
-                TextSpan(
-                  children: <TextSpan>[
-                    TextSpan(text: '${AppLocalizations.of(context).max}: '),
-                    TextSpan(
-                      text: widget
-                          .allGrades
-                          .items[(_currentRangeValues.end as num).toInt()]
-                          .name,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-                key: const Key('boulder-list-grade-max'),
-              ),
-            ],
-          ),
-        ),
-        SizedBox(
-          height: 30,
-          child: RangeSlider(
-            max: widget.allGrades.totalItems - 1,
-            divisions: widget.allGrades.totalItems - 1,
-            values: _currentRangeValues,
+    return Consumer(
+      builder: (context, ref, _) {
+        final notifier = ref.watch(
+          _viewModelProvider(
+            allGrades,
+            context.read<BoulderFilterGradeBloc>().state.grades,
+          ).notifier,
+        );
 
-            onChanged: (values) {
-              setState(() {
-                _currentRangeValues = values;
-              });
-            },
-            onChangeEnd: (values) {
-              if ([values.start, values.end] case [final num x, final num y]) {
-                final startIndex = x.round();
-                final endIndex = y.round();
-                var newValues = <Grade>{};
-                if (startIndex != 0 ||
-                    endIndex != widget.allGrades.totalItems - 1) {
-                  newValues = {
-                    ...widget.allGrades.items.getRange(
-                      startIndex,
-                      endIndex + 1,
-                    ),
-                  };
-                }
-
-                context.read<BoulderFilterGradeBloc>().add(
-                  BoulderFilterGradeEvent(newValues),
-                );
-              } else {
-                throw StateError(
-                  'values.start and values.end should be number',
-                );
-              }
-            },
+        final rangeValues = ref.watch(
+          _viewModelProvider(
+            allGrades,
+            context.read<BoulderFilterGradeBloc>().state.grades,
           ),
-        ),
-      ],
+        );
+        return Column(
+          children: [
+            ListTile(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: '${AppLocalizations.of(context).minGrade}: ',
+                        ),
+                        TextSpan(
+                          text: allGrades
+                              .items[(rangeValues.start as num).toInt()]
+                              .name,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    key: const Key('boulder-list-grade-min'),
+                  ),
+                  Text.rich(
+                    TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(text: '${AppLocalizations.of(context).max}: '),
+                        TextSpan(
+                          text: allGrades
+                              .items[(rangeValues.end as num).toInt()]
+                              .name,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    key: const Key('boulder-list-grade-max'),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+              child: RangeSlider(
+                max: allGrades.totalItems - 1,
+                divisions: allGrades.totalItems - 1,
+                values: rangeValues,
+                onChanged: notifier.onChanged,
+                onChangeEnd: (values) {
+                  context.read<BoulderFilterGradeBloc>().add(
+                    BoulderFilterGradeEvent(
+                      notifier.fromRangeValuesToGrades(values),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      },
     );
+  }
+}
+
+@riverpod
+class _ViewModel extends _$ViewModel {
+  @override
+  RangeValues build(PaginatedCollection<Grade> allGrades, Set<Grade> grades) {
+    final selectedGrades = grades.toList()
+      ..sort((a, b) => a.name.compareTo(b.name));
+
+    if (selectedGrades.isEmpty) {
+      return RangeValues(
+        0,
+        (allGrades.items.length - 1).toDouble(),
+      );
+    } else {
+      final startIndex = allGrades.items.indexWhere(
+        (element) => element.iri == selectedGrades.first.iri,
+      );
+      final endIndex = allGrades.items.indexWhere(
+        (element) => element.iri == selectedGrades.last.iri,
+      );
+      if (startIndex != -1 && endIndex != -1) {
+        return RangeValues(
+          startIndex.toDouble(),
+          endIndex.toDouble(),
+        );
+      } else {
+        return RangeValues(
+          0,
+          (allGrades.items.length - 1).toDouble(),
+        );
+      }
+    }
+  }
+
+  // ignore: use_setters_to_change_properties
+  void onChanged(RangeValues values) {
+    state = values;
+  }
+
+  Set<Grade> fromRangeValuesToGrades(RangeValues values) {
+    final startIndex = values.start.round();
+    final endIndex = values.end.round();
+    var newValues = <Grade>{};
+    if (startIndex != 0 || endIndex != allGrades.totalItems - 1) {
+      newValues = {
+        ...allGrades.items.getRange(
+          startIndex,
+          endIndex + 1,
+        ),
+      };
+    }
+    return newValues;
   }
 }
