57f449c295bf2679d80ab2ce1de3196cf44af323
Jérémy Riverain <riverainjeremy@gmail.com>
2026-05-12 20:39:38 +0200

    🐛 fix provider not found

--- packages/app/lib/ui/boulder/view_models/boulder_message_feedback_view_model.dart
+++ packages/app/lib/ui/boulder/view_models/boulder_message_feedback_view_model.dart
@@ -4,8 +4,11 @@ import 'package:breizh_blok_mobile/domain/repositories/boulder_feedback_reposito
 import 'package:breizh_blok_mobile/ui/boulder/forms/contribute_boulder_message_form.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'boulder_message_feedback_view_model.freezed.dart';
+
+part 'boulder_message_feedback_view_model.g.dart';
 
 class BoulderMessageFeedbackViewModel
     extends Bloc<SubmitContributeFormEvent, BoulderMessageFeedbackState> {
@@ -79,4 +82,60 @@ abstract class BoulderMessageFeedbackState with _$BoulderMessageFeedbackState {
     required bool done,
     required bool error,
   }) = _BoulderMessageFeedbackState;
+}
+
+@riverpod
+class BoulderFeedbackViewModel extends _$BoulderFeedbackViewModel {
+  @override
+  BoulderMessageFeedbackState build({
+    required ContributeBoulderMessageForm form,
+    required BoulderFeedbackRepository boulderFeedbackRepository,
+    required Boulder boulder,
+  }) {
+    return BoulderMessageFeedbackState(
+      form: form,
+      pending: false,
+      done: false,
+      error: false,
+    );
+  }
+
+  Future<void> onSubmit() async {
+    form.markAllAsTouched();
+    if (form.invalid) {
+      return;
+    }
+
+    state = BoulderMessageFeedbackState(
+      form: state.form,
+      pending: true,
+      done: false,
+      error: false,
+    );
+
+    final result = await boulderFeedbackRepository
+        .create(
+          BoulderFeedback(boulder: boulder, message: state.form.message),
+        )
+        .run();
+
+    result.match(
+      (domainException) {
+        state = BoulderMessageFeedbackState(
+          form: state.form,
+          pending: false,
+          done: false,
+          error: true,
+        );
+      },
+      (_) {
+        state = BoulderMessageFeedbackState(
+          form: ContributeBoulderMessageForm(),
+          pending: false,
+          done: true,
+          error: false,
+        );
+      },
+    );
+  }
 }
