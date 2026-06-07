075ce3f60b90fe4513c3fe5f8c3c39de265de15a
Jérémy Riverain <riverainjeremy@gmail.com>
2026-06-07 14:47:31 +0200

    🔧 add method to filter local boulder geo points by area (#205)

--- packages/app/lib/data/data_sources/local/local_boulder_geo_point_data_source.dart
+++ packages/app/lib/data/data_sources/local/local_boulder_geo_point_data_source.dart
@@ -50,4 +50,56 @@ class LocalBoulderGeoPointDataSource {
       );
     }
   }
+
+  Stream<List<BoulderGeoPoint>> watchByArea(int areaId) {
+    try {
+      return (_database.select(
+        _database.boulderGeoPointTable,
+      )..where((u) => u.areaId.equals(areaId))).map(_mapper.toDomain).watch();
+    } catch (e) {
+      throw AppDatabaseException(
+        message: e.toString(),
+      );
+    }
+  }
+
+  Future<void> seedByArea(List<BoulderGeoPoint> boulderGeoPoints) async {
+    try {
+      if (boulderGeoPoints.isEmpty) {
+        return;
+      }
+      final areaIds = boulderGeoPoints.map((u) => u.areaId).toSet();
+
+      if (areaIds.length != 1) {
+        throw Exception(
+          'The geo points are not associated to a single area: $areaIds',
+        );
+      }
+      // During update, foreign constraints must be disabled.
+      // See: https://drift.simonbinder.eu/Migrations/api
+      await _database.setForeignKeysMode(isActive: false);
+      await _database.transaction(() async {
+        await (_database.delete(
+          _database.boulderGeoPointTable,
+        )..where((u) => u.areaId.equals(areaIds.first))).go();
+        await _database.batch(
+          (batch) {
+            batch.insertAll(
+              _database.boulderGeoPointTable,
+              [
+                for (final boulderGeoPoint in boulderGeoPoints)
+                  _mapper.fromDomain(boulderGeoPoint).toCompanion(true),
+              ],
+            );
+          },
+        );
+      });
+    } catch (e) {
+      throw AppDatabaseException(
+        message: e.toString(),
+      );
+    } finally {
+      await _database.setForeignKeysMode(isActive: true);
+    }
+  }
 }
--- packages/app/test/data/data_sources/local/local_boulder_geo_point_data_source_test.dart
+++ packages/app/test/data/data_sources/local/local_boulder_geo_point_data_source_test.dart
@@ -57,6 +57,72 @@ void main() {
       });
     });
 
+    group('watchByArea', () {
+      test('empty list by default', () async {
+        final database = createMemoryDatabase();
+        final dataSource = LocalBoulderGeoPointDataSource(
+          database: database,
+        );
+        await expectLater(
+          dataSource.watchByArea(1),
+          emits(isEmpty),
+        );
+      });
+
+      test('empty list if the area id matches nothing', () async {
+        final database = createMemoryDatabase();
+        final dataSource = LocalBoulderGeoPointDataSource(
+          database: database,
+        );
+
+        await database.insertFixtures(
+          boulderGeoPoints: [fakeBoulderGeoPoint.copyWith(areaId: 2)],
+        );
+        await expectLater(
+          dataSource.watchByArea(1),
+          emits(isEmpty),
+        );
+      });
+
+      test('emits only corresponding areas', () async {
+        final database = createMemoryDatabase();
+        final dataSource = LocalBoulderGeoPointDataSource(
+          database: database,
+        );
+
+        await database.insertFixtures(
+          boulderGeoPoints: [fakeBoulderGeoPoint, fakeBoulderGeoPoint2],
+        );
+
+        expect(
+          fakeBoulderGeoPoint.areaId == fakeBoulderGeoPoint2.areaId,
+          isFalse,
+        );
+        await expectLater(
+          dataSource.watchByArea(fakeBoulderGeoPoint.areaId),
+          emits([fakeBoulderGeoPoint]),
+        );
+      });
+
+      test('Given database throws an exception, '
+          'When watchByArea is called, '
+          'Then it throws a $AppDatabaseException', () async {
+        final database = MockAppDatabase();
+        final dataSource = LocalBoulderGeoPointDataSource(
+          database: database,
+        );
+        when(
+          () => database.select(database.boulderGeoPointTable),
+        ).thenThrow(Exception('fake'));
+        expect(
+          () => dataSource.watchByArea(1),
+          throwsA(
+            isA<AppDatabaseException>(),
+          ),
+        );
+      });
+    });
+
     group('seed', () {
       test('seeds entities from db', () async {
         final database = createMemoryDatabase();
@@ -127,6 +193,132 @@ void main() {
           ),
         );
       });
+    });
+
+    group('seedByArea', () {
+      test('do nothing if empty array passed to seedByArea', () async {
+        final database = createMemoryDatabase();
+        final dataSource = LocalBoulderGeoPointDataSource(
+          database: database,
+        );
+        await database.insertFixtures(
+          boulderGeoPoints: [
+            fakeBoulderGeoPoint,
+          ],
+        );
+        await expectLater(
+          dataSource.seedByArea(
+            [],
+          ),
+          completes,
+        );
+
+        await expectLater(
+          dataSource.watchAll(),
+          emits(
+            const [
+              fakeBoulderGeoPoint,
+            ],
+          ),
+        );
+      });
+
+      test('replaces geo points belonging to a specific area', () async {
+        final database = createMemoryDatabase();
+        final dataSource = LocalBoulderGeoPointDataSource(
+          database: database,
+        );
+        await database.insertFixtures(
+          boulderGeoPoints: [
+            fakeBoulderGeoPoint.copyWith(areaId: 1, lat: 1),
+            fakeBoulderGeoPoint2.copyWith(areaId: 2),
+          ],
+        );
+        await expectLater(
+          dataSource.seedByArea(
+            [
+              fakeBoulderGeoPoint.copyWith(areaId: 1, lat: 2),
+              fakeBoulderGeoPoint.copyWith(areaId: 1, lat: 3, id: 4566),
+            ],
+          ),
+          completes,
+        );
+
+        await expectLater(
+          dataSource.watchAll(),
+          emits(
+            [
+              fakeBoulderGeoPoint.copyWith(areaId: 1, lat: 2),
+              fakeBoulderGeoPoint2.copyWith(areaId: 2),
+              fakeBoulderGeoPoint.copyWith(areaId: 1, lat: 3, id: 4566),
+            ],
+          ),
+        );
+      });
+
+      test(
+        'delete existing geo points belonging to a specific area '
+        'before inserting the new ones',
+        () async {
+          final database = createMemoryDatabase();
+          final dataSource = LocalBoulderGeoPointDataSource(
+            database: database,
+          );
+          await database.insertFixtures(
+            boulderGeoPoints: [
+              fakeBoulderGeoPoint.copyWith(areaId: 1, lat: 1),
+              fakeBoulderGeoPoint2.copyWith(areaId: 2),
+            ],
+          );
+          await expectLater(
+            dataSource.seedByArea(
+              [
+                fakeBoulderGeoPoint.copyWith(areaId: 1, lat: 3, id: 4566),
+              ],
+            ),
+            completes,
+          );
+
+          await expectLater(
+            dataSource.watchAll(),
+            emits(
+              [
+                fakeBoulderGeoPoint2.copyWith(areaId: 2),
+                fakeBoulderGeoPoint.copyWith(areaId: 1, lat: 3, id: 4566),
+              ],
+            ),
+          );
+        },
+      );
+
+      test(
+        'throw exception if multiple area IDs passed to the seedByArea method',
+        () async {
+          final database = createMemoryDatabase();
+          final dataSource = LocalBoulderGeoPointDataSource(
+            database: database,
+          );
+
+          expect(
+            () => dataSource.seedByArea(
+              [
+                fakeBoulderGeoPoint.copyWith(areaId: 1),
+                fakeBoulderGeoPoint2.copyWith(areaId: 2),
+              ],
+            ),
+            throwsA(
+              isA<AppDatabaseException>().having(
+                (e) => e.message,
+                'message',
+                equals(
+                  'Exception: '
+                  'The geo points are not associated to a single area: {1, 2}',
+                ),
+              ),
+            ),
+          );
+        },
+      );
     });
   });
 }
